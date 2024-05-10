a855ce9b460b3bfa2ec4105edd87596111bfed54
eigenscribe <111261353+eigenscribe@users.noreply.github.com>
2024-05-09 20:39:48 -0400

    please work

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # projExamples
-Example code
+
+[5 Good Python Habits](https://www.youtube.com/watch?v=I72uD8ED73U)
---
+++ module.py
@@ -0,0 +1,6 @@
+import time
+
+def connect() -> None: 
+    print('Connecting to the internet... ⏳')
+    time.sleep(3)
+    print('You are connected! 🥳')

d6bd9543ce5d52178263a18398d3ef8c7147fdc3
eigenscribe <111261353+eigenscribe@users.noreply.github.com>
2024-05-11 19:57:04 -0400

    Delete module.py

--- module.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def connect() -> None: 
-    print('Connecting to the internet... ⏳')
-    time.sleep(3)
-    print('You are connected! 🥳')
-    
-if __name__ == '__main__':
-    connect()

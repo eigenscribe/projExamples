7d578368c58c531b838249125f78cae1dbf36bef
eigenscribe <111261353+eigenscribe@users.noreply.github.com>
2025-08-29 22:04:11 -0400

    Update main.py

--- 5 Good Python Habits/Habit 4/main.py
+++ 5 Good Python Habits/Habit 4/main.py
@@ -1,4 +1,12 @@
 # Habit 4 - Type Annotations
+number1: int = 10      # ✔️
+number2: int = 10.0    # 🚫 will generate a warning since 10.0 is not an integer
+number3: float = 10.0  # ✔️
+
 
 def upper_everything(elements):
   return [element.upper() for element in elements]
+
+list1: list[str] = upper_everything(['Frodo', 'Sam', 'Merry', 'Pippen'])    # ✔️
+list2: list[int] = upper_everything(['Frodo', 'Sam', 'Merry', 'Pippen'])    # 🚫
+list3: list[int] = upper_everything([1, 2, 3, 4])                           # 🚫
